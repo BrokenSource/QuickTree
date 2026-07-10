736ad917efc3c44fae33b3f564640b03789bc557
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2026-07-09 21:51:54 -0300

    feat: sketching implementation, generics, basic project information

--- quicktree/__init__.py
+++ quicktree/__init__.py
@@ -1,20 +1,22 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Iterable, Union
 
-from attrs import define
+from attrs import Factory, define
+
+type Time = float
 
 # ---------------------------------------------------------------------------- #
 
 @define(frozen=True, eq=False)
-class Interval:
+class Interval[T: Any]:
 
-    data: Any
+    data: T
     """Interval data"""
 
-    a: float
+    A: Time
     """Interval start"""
 
-    b: float
+    B: Time
     """Interval end"""
 
     def __hash__(self) -> int:
@@ -22,33 +24,86 @@ class Interval:
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, Interval):
-            return False
+            raise TypeError
         return self.data is other.data
 
 # ---------------------------------------------------------------------------- #
 
 @define
-class _IntervalTree(ABC):
+class _IntervalTree[T: Any](ABC):
     """Trait that all interval trees must implement"""
 
     @abstractmethod
-    def at(self, point: float) -> list[Interval]:
+    def at(self, time: Time) -> Iterable[Interval[T]]:
         ...
 
     @abstractmethod
-    def add(self, interval: Interval) -> None:
+    def add(self, /, interval: Interval[T]) -> None:
         ...
+
+    @abstractmethod
+    def overlap(self, A: Time, B: Time) -> Iterable[Interval[T]]:
+        ...
+
+    def __getitem__(self,
+        time: Union[Time, slice[Time]]
+    ) -> Iterable[Interval[T]]:
+
+        # Syntax: tree[a:b]
+        if isinstance(time, slice):
+            yield from self.overlap(
+                A=time.start,
+                B=time.stop,
+            )
+
+        # Syntax: tree[a]
+        else:
+            yield from self.at(time)
+
+    def __setitem__(self,
+        time: Union[Time, slice[Time]],
+        data: T
+    ) -> None:
+        # Syntax: tree[a:b]
+        if isinstance(time, slice):
+            self.add(Interval(
+                A=time.start,
+                B=time.stop,
+                data=data,
+            ))
+
+        else:
+            # Syntax: tree[a]
+            self.add(Interval(
+                A=time,B=time,
+                data=data,
+            ))
 
 # ---------------------------------------------------------------------------- #
 
 @define
-class QuickTree(_IntervalTree):
+class QuickTree[T: Any](_IntervalTree):
 
-    intervals: list[Interval]
+    intervals: list[Interval[T]] = Factory(list)
     """List of intervals"""
 
-    granularity: int = 1
+    granularity: Time = 1
     """Bin size for intervals"""
+
+    def at(self, time: Time) -> Iterable[Interval]:
+        for I in self.intervals:
+            if I.A <= time < I.B:
+                yield I
+
+    def add(self, /, interval: Interval) -> None:
+        self.intervals.append(interval)
+
+    def overlap(self, A: Time, B: Time) -> Iterable[Interval]:
+        for I in self.intervals:
+            if I.B <= A or B <= I.A:
+                yield I
+
+# ---------------------------------------------------------------------------- #
 
 # Idea: Only query bins
 @define
--- readme.md
+++ readme.md
@@ -1,1 +1,25 @@
-A fast interval tree data structure for python (work in progress)
+> [!WARNING]
+> Work in progress
+
+<div align="center">
+  <h1>QuickTree</h1>
+  <span>🌲 Fast interval trees for python 🌲</span>
+</div>
+
+## 📦 Description
+
+A fast [Interval Tree](https://en.wikipedia.org/wiki/Interval_tree) data structure for python:
+
+- [x] **Typed** Uses generics in intervals and tree classes
+- [x] **Fast**: Binned sorage reduces problem complexity
+
+## 📦 Usage
+
+Simply add the [`quicktree`](https://pypi.org/project/quicktree/) PyPI package to your `pyproject.toml` and use it:
+
+```toml
+[project]
+dependencies = ["quicktree"]
+```
+
+Todo: examples
